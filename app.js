dacf7bbac0ccedc99e1684a869ad0793fea2e3c6
nateos <sodjanathan@gmail.com>
2023-12-06 18:40:57 +0000

    chore: app.use(userRoute) + remove and perm

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-const rateLimiter = require("express-rate-limit");
-const helmet = require("helmet");
+/* const rateLimiter = require("express-rate-limit");
+ */ const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -22,18 +22,17 @@ const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const callRouter = require("./routes/callRoutes");
 
-
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.set("trust proxy", 1);
-app.use(
+/* app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
     max: 60,
   }),
-);
+); */
 app.use(helmet());
 app.use(cors());
 app.use(xss());
@@ -47,7 +46,7 @@ app.use(fileUpload());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/calls", callRouter);
-
+app.use("/api/v1/users", userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
